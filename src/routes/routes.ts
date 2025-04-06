2671e176519cae3b3f4813ae32e6503d6e491535
sangam5756 <sangammunde3@gmail.com>
2025-04-06 18:36:19 +0530

    routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,24 +4,30 @@ import { emailQueue } from "../jobs/emailQueue";
 export const router = expres.Router();
 
 router.post("/send", async (req: Request, res: Response): Promise<any> => {
-  const { to, subject, text, html } = req.body;
-  await emailQueue.add(
-    "send_email",
-    { to, subject, text, html },
-    {
-      attempts: 5,
-      backoff: {
-        type: "exponential",
-        delay: 1000,
-      },
-      removeOnComplete: true,
-      removeOnFail: {
-        count: 5,
-      },
-    }
-  );
+  try {
+    const { to, subject, text, html } = req.body;
+    await emailQueue.add(
+      "send_email",
+      { to, subject, text, html },
+      {
+        attempts: 5,
+        backoff: {
+          type: "exponential",
+          delay: 1000,
+        },
+        removeOnComplete: true,
+        removeOnFail: {
+          count: 5,
+        },
+      }
+    );
 
-  res.status(200).json({
-    message: "Email job successfully added to queue",
-  });
+    res.status(200).json({
+      message: "Email job successfully added to queue",
+    });
+  } catch (error: any) {
+    console.error("Failed to add job to queue:", error.message);
+
+    res.status(500).json({ error: "Failed to add email job to the queue" });
+  }
 });
